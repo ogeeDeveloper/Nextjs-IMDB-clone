0aaf6a01898a4ac3d6dee2614abbdd828151293f
ogeeDeveloper <omarogrant202@hotmail.com>
2022-02-17 17:33:27 -0500

    Created header icon component and added the icons from heroicons

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,21 @@
+import Image from "next/image"
+import HeaderIcon from "./HeaderIcon";
+import { HiHome } from "react-icons/hi";
+import { HiUserCircle } from "react-icons/hi";
+import { HiOutlinePhone } from "react-icons/hi";
+import { HiInformationCircle } from "react-icons/hi";
+
+
 const Header=()=>{
     return(
         <div>
-            <h1>Header</h1>
+            <div className="">
+                <HeaderIcon Icon={HiHome} title="home" />
+                <HeaderIcon Icon={HiUserCircle} title="account" />
+                <HeaderIcon Icon={HiOutlinePhone} title="contact" />
+                <HeaderIcon Icon={HiInformationCircle} title="about" />
+            </div>
+            <Image src="https://clipground.com/images/imdb-icon-clipart-7.png" width={100} height={100} />
         </div>
     )
 }
