9f8d56cc2b53450140bcc9e76e012c8968ed82db
ogeeDeveloper <omarogrant202@hotmail.com>
2022-02-18 12:00:21 -0500

    Styled the header

--- components/Header.js
+++ components/Header.js
@@ -8,14 +8,14 @@ import { HiInformationCircle } from "react-icons/hi";
 
 const Header=()=>{
     return(
-        <div>
-            <div className="">
+        <div className="bg-gray-600 flex flex-col items-center p-6 select-none sm:flex-row justify-between">
+            <div className="text-gray-200 flex">
                 <HeaderIcon Icon={HiHome} title="home" />
                 <HeaderIcon Icon={HiUserCircle} title="account" />
                 <HeaderIcon Icon={HiOutlinePhone} title="contact" />
                 <HeaderIcon Icon={HiInformationCircle} title="about" />
             </div>
-            <Image src="https://clipground.com/images/imdb-icon-clipart-7.png" width={100} height={100} />
+            <Image src="https://clipground.com/images/imdb-icon-clipart-7.png" width={100} height={100} className="cursor-pointer active:brightness-110"/>
         </div>
     )
 }
